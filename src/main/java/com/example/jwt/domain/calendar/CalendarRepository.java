79cea83f25a08765dd01bf165a61fbd972e80bc9
4dZombie <adrianfrei2000@gmail.com>
2023-12-06 10:42:55 +0100

    Calendar added user connected

---
+++ src/main/java/com/example/jwt/domain/calendar/CalendarRepository.java
@@ -0,0 +1,8 @@
+package com.example.jwt.domain.calendar;
+
+import com.example.jwt.core.generic.ExtendedRepository;
+
+import java.util.UUID;
+
+public interface CalendarRepository extends ExtendedRepository<Calendar>{
+}
---
+++ src/main/java/com/example/jwt/domain/calendar/CalendarService.java
@@ -0,0 +1,6 @@
+package com.example.jwt.domain.calendar;
+
+import com.example.jwt.core.generic.ExtendedService;
+
+public interface CalendarService extends ExtendedService<Calendar> {
+}
